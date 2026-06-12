d78569c8306907c1ecc9fad4300b68df6794e8fa
성태경 <taegyeong0320@naver.com>
2026-06-12 22:49:24 +0900

    2-2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthNavigation from './src/navigations/AuthNavigation';
+import DrawerNavigation from './src/navigations/DrawerNavigation';
 
 function App() {
-  return <AuthNavigation />;
+  // return <AuthNavigation />;
+  return <DrawerNavigation />;
 }
 
 const styles = StyleSheet.create({});
---
+++ src/screens/calendar/CalendarScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function CalendarScreen() {
+  return (
+    <SafeAreaView>
+      <Text>CalendarScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default CalendarScreen;
