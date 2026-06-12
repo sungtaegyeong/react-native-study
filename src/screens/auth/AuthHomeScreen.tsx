7065ce5b5d6ff95061c23e2d53acd4b6493dfec3
성태경 <taegyeong0320@naver.com>
2026-06-12 15:38:24 +0900

    2-1

--- src/screens/auth/AuthHomeScreen.tsx
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 function AuthHomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView>
-      <Text>AuthHomeScreen</Text>
+      <Text onPress={() => navigation.navigate('Login')}>로그인으로 이동</Text>
     </SafeAreaView>
   );
 }
