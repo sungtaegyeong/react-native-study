b251cb24146d91d2d755365b51b4495b1f0f554a
성태경 <taegyeong0320@naver.com>
2026-06-12 13:56:48 +0900

    2-0

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AuthNavigation from './src/navigations/AuthNavigation';
 
 function App() {
-  return (
-    <View>
-      <Text>Matzip</Text>
-    </View>
-  );
+  return <AuthNavigation />;
 }
 
 const styles = StyleSheet.create({});
---
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function AuthHomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>AuthHomeScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
