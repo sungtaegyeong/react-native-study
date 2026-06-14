e2503ab1bb686419f969cf62eef4962af2dcedda
성태경 <taegyeong0320@naver.com>
2026-06-15 02:25:52 +0900

    2-4

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DrawerButton from '../../components/DrawerButton';
 
 function MapHomeScreen() {
   return (
     <SafeAreaView>
       <Text>MapHomeScreen</Text>
+      <DrawerButton />
     </SafeAreaView>
   );
 }
