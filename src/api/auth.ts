b908a17adbf8ba013671cd082f3c34ab0acdc5e8
성태경 <taegyeong0320@naver.com>
2026-06-17 14:07:21 +0900

    3-8

---
+++ src/api/auth.ts
@@ -0,0 +1,56 @@
+import { Profile } from '@/types/domain';
+import { getEncryptStorage } from '@/utils/encryptStorage';
+import axiosInstance from './axios';
+
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+async function postSignup({ email, password }: RequestUser): Promise<void> {
+  await axiosInstance.post('/auth/signup', {
+    email,
+    password,
+  });
+}
+
+type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+async function postLogin({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> {
+  const { data } = await axiosInstance.post('/auth/signin', {
+    email,
+    password,
+  });
+
+  return data;
+}
+
+async function getProfile(): Promise<Profile> {
+  const { data } = await axiosInstance.get('/auth/me');
+
+  return data;
+}
+
+async function getAccessToken(): Promise<ResponseToken> {
+  const refreshToken = await getEncryptStorage('refreshToken');
+
+  const { data } = await axiosInstance.get('/auth/refresh', {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+
+  return data;
+}
+
+async function logout() {
+  await axiosInstance.post('/auth/logout');
+}
+
+export { postSignup, postLogin, getProfile, getAccessToken, logout };
